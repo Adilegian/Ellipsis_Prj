c6c79fbf8355589e0433916ba12103804bf2c865
Jengy Matantsev <mellacita@gmail.com>
2023-05-17 21:52:03 -0700

    laser folder

---
+++ Ellipsis/Assets/Laser/Scripts/lbeam.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class lbeam : MonoBehaviour 
+{
+
+	public GameObject beam;
+	public GameObject lights;
+
+	void Start()
+	{
+		beam.SetActive(false);
+		lights.SetActive(false);
+	}
+
+	void Update()
+	{
+		beamOn();
+		beamOff();
+		LightsOn();
+		LightsOff();
+	}
+
+	void beamOn()
+	{
+		if (Input.GetMouseButtonDown(0))
+        {
+        	beam.SetActive(true);
+        }
+	}
+
+	void beamOff()
+	{
+		if(Input.GetMouseButtonUp(0))
+		{
+			beam.SetActive(false);
+		}
+	}
+
+	void LightsOn()
+	{
+		if (Input.GetMouseButtonDown(1))
+        {
+        	lights.SetActive(true);
+        }
+	}
+
+	void LightsOff()
+	{
+		if (Input.GetMouseButtonUp(1))
+        {
+        	lights.SetActive(false);
+        }
+	}
+}
---
+++ Ellipsis/Assets/Laser/Scripts/sbeam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sbeam : MonoBehaviour 
+{
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+    
+    void Start() 
+    {
+        rend = GetComponent<Renderer>();
+    }
+    
+    void Update() {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
