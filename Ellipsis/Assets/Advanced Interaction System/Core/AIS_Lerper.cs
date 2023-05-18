bc3633d55f7fa7fc19d45e6190b6b822edb55636
Jengy Matantsev <mellacita@gmail.com>
2023-05-17 21:25:42 -0700

    Advanced Interaction System folder with reduced PNG file resolution

---
+++ Ellipsis/Assets/Advanced Interaction System/Core/AIS_Lerper.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+
+public class AIS_Lerper : MonoBehaviour
+{
+    [Tooltip("The Transform of the object to lerp to")]
+	[HideInInspector]
+	public Transform EndPos;
+	
+	[Header("LERPING TIMES")]
+	[Space(20)]
+
+	[Tooltip("The amount of time it takes for a lerp to finish. The greater the number the slower it is. 300 is 3 seconds and so on")]
+	[HideInInspector]
+	public float lerpTime = 5f;
+	
+	[Tooltip("The amount of time it takes for a rotation lerp to finish")]
+	[HideInInspector]
+	public float QuatLerpTime = 5f;
+	
+	[Tooltip("Enable/disable rotation lerping")]
+	[HideInInspector]
+	public bool LerpRotation = true;
+
+	[Header("POSITIONAL OFFSETS")]
+	[Space(20)]
+	[HideInInspector]
+	public float XOffset = 0f,
+    YOffset = 0f,
+    ZOffset = 0f;
+
+	[Header("ROTATIONAL OFFSETS")]
+	[Space(20)]
+	[HideInInspector]
+	public float XOffsetQuart = 0f,
+	YOffsetQuart = 0f,
+	ZOffsetQuart = 0f;
+	
+	private float currentLerpTime = 0f;
+	private float perc;
+	private float Quatperc;
+	private bool _lerp;
+	
+	[HideInInspector]
+	public bool IsActive = false;
+    
+	[HideInInspector]
+    public bool ShouldStopLerp = false;
+	
+	private bool ShouldStopQuatLerp = false;
+
+	AdvancedInteractionSystem ais;
+
+	//property to trigger the lerping function
+	public bool StartLerp {
+		get{ return _lerp; }
+
+		set{
+			_lerp = value;
+			if (_lerp){
+				currentLerpTime = 0f;
+				IsActive = true;
+				ShouldStopLerp = false;
+				ShouldStopQuatLerp = false;
+				Lerping();
+			}
+		}
+	}
+
+	void Start()
+	{
+		ais = GetComponent<AdvancedInteractionSystem>();
+	}
+	
+	//Update is called once per frame
+	void Update () {
+		if(IsActive){
+			Lerping ();
+		}
+	}
+
+	//the main lerping method
+	void Lerping(){
+		
+		//increment timer once per frame
+		currentLerpTime += Time.deltaTime;
+		if (currentLerpTime > lerpTime) {
+			currentLerpTime = lerpTime;
+		}
+
+		//lerp!
+		perc = currentLerpTime / lerpTime;
+		Quatperc = currentLerpTime / QuatLerpTime;
+
+		if (!ShouldStopLerp)
+			transform.position = Vector3.Lerp (transform.position, EndPos.position + new Vector3(XOffset, YOffset, ZOffset), perc);
+
+		//only if checked, lerp rotation as well
+		if (LerpRotation && !ShouldStopQuatLerp)
+			transform.rotation = Quaternion.Lerp (transform.rotation, EndPos.rotation * Quaternion.Euler(XOffsetQuart, YOffsetQuart, ZOffsetQuart), Quatperc);
+
+		//if distance between two goals of lerp is smaller than threshold then stop lerp
+		if (Vector3.Distance (transform.position, EndPos.position) <= 0.01f){
+			ShouldStopLerp = true;
+			//if rotation lerping is enabled
+			if (LerpRotation) {
+				//if angle between two quaternions is smaller than threshold then stop
+				if (Quaternion.Angle(transform.rotation, EndPos.rotation) <= 0.01f){
+					ShouldStopQuatLerp = true;
+					IsActive = false;
+					Destroy(ais.go);
+					ais.interactable = true;
+					this.enabled = false;
+				}
+			//if lerp rotation disabled then stop the method
+			}else {
+				IsActive = false;
+				Destroy(ais.go);
+				ais.interactable = true;
+				this.enabled = false;
+			}
+		}
+	}
+}
---
+++ Ellipsis/Assets/Advanced Interaction System/Editor/AIS_Inspector.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(AdvancedInteractionSystem))]
+
+public class AIS_Inspector : Editor
+{
+    SerializedProperty StartPositonAndRotation,
+    MainPosition,
+    MainRotation,
+
+    SecondaryPosition,
+    SecondaryRotation,
+    
+    OpenAudio,
+    CloseAudio,
+    LockedAudio,
+    
+    OpenSpeed,
+    CloseSpeed,
+    
+    OpenRotationSpeed,
+    CloseRotationSpeed,
+    
+    locked,
+    ConnectedScripts;
+
+    void OnEnable()
+    {
+        StartPositonAndRotation = serializedObject.FindProperty("StartPositionAndRotation");
+        MainPosition = serializedObject.FindProperty("MainPosition");
+        MainRotation = serializedObject.FindProperty("MainRotation");
+
+        SecondaryPosition = serializedObject.FindProperty("SecondaryPosition");
+        SecondaryRotation = serializedObject.FindProperty("SecondaryRotation");
+
+        OpenAudio = serializedObject.FindProperty("OpenAudio");
+        CloseAudio = serializedObject.FindProperty("CloseAudio");
+        LockedAudio = serializedObject.FindProperty("LockedAudio");
+
+        OpenSpeed = serializedObject.FindProperty("OpenSpeed");
+        CloseSpeed = serializedObject.FindProperty("CloseSpeed");
+
+        OpenRotationSpeed = serializedObject.FindProperty("OpenRotationSpeed");
+        CloseRotationSpeed = serializedObject.FindProperty("CloseRotationSpeed");
+    
+        locked = serializedObject.FindProperty("locked");
+        ConnectedScripts = serializedObject.FindProperty("ConnectedScripts");
+    }
+
+    public override void OnInspectorGUI(){
+        GUILayout.Box(Resources.Load("AIS_background") as Texture, GUILayout.Width(400), GUILayout.Height(215));
+        AdvancedInteractionSystem script = (AdvancedInteractionSystem)target;
+
+        EditorGUILayout.HelpBox("If you like this asset please don't forget to leave a nice review. It helps alot.", MessageType.Info);
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Main State", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(StartPositonAndRotation, new GUIContent("Start Position And Rotation", "Automatically get the main position and rotation of the object on start"));
+
+        EditorGUI.BeginDisabledGroup(script.StartPositionAndRotation == true);
+            EditorGUILayout.PropertyField(MainPosition, new GUIContent("Main Position", "The main transform position of the object. If the object isn't moving on game start then set it exactly the same as it's current transform position."));
+            EditorGUILayout.PropertyField(MainRotation, new GUIContent("Main Rotation", "The main rotation of the object. If the object isn't rotating on game start then set it exactly the same as it's current transform rotation"));
+        EditorGUI.EndDisabledGroup();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Secondary State", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(SecondaryPosition, new GUIContent("Secondary Position", "The secondary position of the object when it's interacted with"));
+        EditorGUILayout.PropertyField(SecondaryRotation, new GUIContent("Secondary Rotation", "The secondary rotation of the object when it's interacted with"));
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Audios", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(OpenAudio, new GUIContent("Open Audio", "The audio played when the object is moving to the secondary state"));
+        EditorGUILayout.PropertyField(CloseAudio, new GUIContent("Close Audio", "The audio played when the object is moving back to the main state"));
+        EditorGUILayout.PropertyField(LockedAudio, new GUIContent("Locked Audio", "The audio played when the object is locked but interacted with"));
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Speeds", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(OpenSpeed, new GUIContent("Open Speed", "Speed when moving from main to secondary state"));
+        EditorGUILayout.PropertyField(CloseSpeed, new GUIContent("Close Speed", "Speed when moving from secondary to main state"));
+        
+        EditorGUILayout.Space();
+
+        EditorGUILayout.PropertyField(OpenRotationSpeed, new GUIContent("Open Rotation Speed", "Speed of rotation when moving from main to secondary state"));
+        EditorGUILayout.PropertyField(CloseRotationSpeed, new GUIContent("Close Rotation Speed", "Speed of rotation when moving from secondary to main state"));
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Miscellaneous", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(locked, new GUIContent("Locked", "Can the object be opened or is it locked?"));
+        EditorGUILayout.PropertyField(ConnectedScripts, new GUIContent("Connected Scripts", "Attach the A.I.S. script of other parts to move different parts together. Like two push doors."), true);
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
